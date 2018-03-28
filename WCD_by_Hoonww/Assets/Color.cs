df03d4f4ce418c6f165ab66803ad0d39725ec95b
DESKTOP-PJF3N48\jh981 <jh981113@naver.com>
2018-03-28 20:10:37 +0900

    Initial commit

---
+++ WCD_by_Hoonww/Assets/Color.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct CMYK
+{
+    public double c, m, y, k;
+};
+
+struct RGB
+{
+    public double r, g, b;
+};
+
+struct HSV
+{
+    public double h, s, v;
+}
+
+public class Color{
+    RGB CtoR(CMYK x)
+    {
+        RGB ans;
+
+        ans.r = 255 * (1 - x.c) * (1 - x.k);
+
+        return ans;
+    }
+}
