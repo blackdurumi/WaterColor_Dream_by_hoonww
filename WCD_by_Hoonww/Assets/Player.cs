0288a25bf3a12c01d05bdcbd5eddf6b4760ac555
DESKTOP-PJF3N48\jh981 <jh981113@naver.com>
2018-04-01 02:27:30 +0900

    Player Bouncing #2(multiple call problem solved)

--- WCD_by_Hoonww/Assets/Player.cs
+++ WCD_by_Hoonww/Assets/Player.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-
-    private void Start()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(50, 0, 0));
-    }
-
+    bool isColliding;
     private void OnCollisionEnter(Collision collision)
     {
+        if (isColliding) return;
+        isColliding = true;
         if (collision.collider.tag == "Tile")
         {
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.AddForce(new Vector3(0, 450, 0));
         }
+    }
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(50, 0, 0));
+    }
+
+    void Update()
+    {
+        isColliding = false;
     }
 }
