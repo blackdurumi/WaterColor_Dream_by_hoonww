f29b016b8638f648bd654f499e0a239dce3058f5
DESKTOP-PJF3N48\jh981 <jh981113@naver.com>
2018-04-11 17:53:32 +0900

    Player Color Changing #1

--- WCD_by_Hoonww/Assets/Player.cs
+++ WCD_by_Hoonww/Assets/Player.cs
@@ -13,6 +13,14 @@ public class Player : MonoBehaviour {
         {
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.AddForce(new Vector3(0, 450, 0));
+
+            UnityEngine.Color ChangedColor;
+            ChangedColor = GetComponent<MeshRenderer>().materials[0].color + collision.collider.GetComponent<MeshRenderer>().materials[0].color;
+                /*new UnityEngine.Color(
+                (GetComponent<MeshRenderer>().materials[0].GetColor. + collision.collider.GetComponent<MeshRenderer>().materials[0].GetColor(0)) / 2.0f,
+                (GetComponent<MeshRenderer>().materials[0].GetColor(1) + collision.collider.GetComponent<MeshRenderer>().materials[0].GetColor(1)) / 2.0f,
+                (GetComponent<MeshRenderer>().materials[0].GetColor(2) + collision.collider.GetComponent<MeshRenderer>().materials[0].GetColor(2)) / 2.0f);*/
+            GetComponent<MeshRenderer>().materials[0].SetColor("_Color", ChangedColor);
         }
     }
 
@@ -20,6 +28,8 @@ public class Player : MonoBehaviour {
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(50, 0, 0));
+        GetComponent<MeshRenderer>().materials[0].SetColor("_Color", new UnityEngine.Color(1.0f, 1.0f, 1.0f));
+        
     }
 
     void Update()
